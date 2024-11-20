2589ed7ae108459fc56a871285ebe09e61620c7c
shanmugapriyan3089 <priyanr1999@gmail.com>
2024-11-21 04:13:32 +0530

    Added the code

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Calendar from './Calendar'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Calendar />
+      </div>
+     
+    </>
+  )
+}
+
+export default App
